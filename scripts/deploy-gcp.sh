d4336a065c846cde34b9edf9a8ee39557392eb77
Francisco Ribeiro <francisco@francisco.local>
2020-04-01 18:32:05 -0300

    Init the project again

--- scripts/deploy-gcp.sh
+++ scripts/deploy-gcp.sh
@@ -12,11 +12,11 @@ MGW_BASE_PATH="$PWD/$GW_BIN"
 MGW_BIN="$PWD/$GW_BIN/bin"
 
 echo "${MGW_BIN}"
-mkdir -p myapi/target/gen
-chmod -R 777 myapi/ 
-ls -ltr myapi
+#mkdir -p myapi/target/gen
+#chmod -R 777 myapi/ 
+#ls -ltr myapi
 
-mv myapi/gen/api_definitions/*.yaml myapi/api_definitions/
+#mv myapi/gen/api_definitions/*.yaml myapi/api_definitions/
 
 $MGW_BIN/micro-gw build myapi --docker-image "fjunior87/myapi-mgw:latest" --docker-base-image wso2/wso2micro-gw:3.1.0
 

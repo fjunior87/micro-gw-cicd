ae8514efcbcd06829b9513113f479cafa2e3c9c4
Francisco Ribeiro <francisco@francisco.local>
2020-04-01 16:01:36 -0300

    Added Products API Project

--- scripts/deploy-gcp.sh
+++ scripts/deploy-gcp.sh
@@ -1,4 +1,4 @@
-echo "Hello traviss" 
+
 
 GW_BIN="wso2am-micro-gw-toolkit-linux-3.1.0"
 #GW_BIN="wso2am-micro-gw-toolkit-3.0.1"
@@ -13,12 +13,15 @@ MGW_BIN="$PWD/$GW_BIN/bin"
 
 echo "${MGW_BIN}"
 
-$MGW_BIN/micro-gw build myapi --docker-image fjunior87/myapi-mgw:v1 --docker-base-image wso2/wso2micro-gw:3.1.0
+$MGW_BIN/micro-gw build myapi --docker-image fjunior87/myapi-mgw:$SHA1 --docker-base-image wso2/wso2micro-gw:3.1.0
 
 docker image ls
 
-docker push fjunior87/myapi-mgw:v1
+docker push fjunior87/myapi-mgw:$SHA1
+docker push fjunior87/myapi-mgw:latest
 
 # Apply Kubernetes configs
+kubectl delete -f kubernetes/
 kubectl apply -f kubernetes/
+
 

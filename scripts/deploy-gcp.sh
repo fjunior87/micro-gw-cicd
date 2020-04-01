05e282af56bd77246b83650578059da1590a61fb
Francisco Ribeiro <francisco@francisco.local>
2020-04-01 08:57:56 -0300

    Remove verbose logs and test micro-gw command

--- scripts/deploy-gcp.sh
+++ scripts/deploy-gcp.sh
@@ -4,24 +4,24 @@ GW_BIN="wso2am-micro-gw-toolkit-linux-3.1.0"
 #GW_BIN="wso2am-micro-gw-toolkit-3.0.1"
 
 echo "Downloading micro-gw"
-wget "https://github.com/wso2/product-microgateway/releases/download/v3.1.0/${GW_BIN}.zip"
+wget -q "https://github.com/wso2/product-microgateway/releases/download/v3.1.0/${GW_BIN}.zip"
 
-unzip "${GW_BIN}.zip"
+unzip -q "${GW_BIN}.zip"
 
 MGW_BASE_PATH="$PWD/$GW_BIN"
 MGW_BIN="$PWD/$GW_BIN/bin"
 
 echo "${MGW_BIN}"
 
-cat deployment.toml
+#cat deployment.toml
 
 #Update deployment.toml
 #cd ..
 
 sed -i "s#TOOLKIT_BASE#$MGW_BASE_PATH#g" deployment.toml
-
-cat deployment.toml
-
+ls -ltr myapi
+#cat deployment.toml
+$MGW_BIN/micro-gw 
 $MGW_BIN/micro-gw build myapi #--deployment-config deployment.toml
 
 docker image ls

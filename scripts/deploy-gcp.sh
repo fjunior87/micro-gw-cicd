90de42d2cd57a2c0fee36ef2def90cb439d798d6
Francisco Ribeiro <francisco@francisco.local>
2020-03-31 22:09:35 -0300

    Fixed zip name

--- scripts/deploy-gcp.sh
+++ scripts/deploy-gcp.sh
@@ -1,6 +1,6 @@
 echo "Hello traviss" 
 
-GW_BIN="wso2am-micro-gw-toolkit-linux-3.1.0.zip"
+GW_BIN="wso2am-micro-gw-toolkit-linux-3.1.0"
 #GW_BIN="wso2am-micro-gw-toolkit-3.0.1.zip"
 
 echo "Downloading micro-gw"
@@ -10,6 +10,8 @@ unzip "${GW_BIN}.zip"
 
 MGW_BASE_PATH="$PWD/$GW_BIN"
 MGW_BIN="$PWD/$GW_BIN/bin"
+
+echo "${MGW_BIN}"
 
 $MGW_BIN/micro-gw
 

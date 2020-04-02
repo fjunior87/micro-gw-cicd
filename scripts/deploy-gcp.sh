5824277143ac26204f507492f87319a41a6561d2
Francisco Ribeiro <francisco@francisco.local>
2020-04-02 09:19:27 -0300

    Add logic for caching micro-gw toolkit download

--- scripts/deploy-gcp.sh
+++ scripts/deploy-gcp.sh
@@ -3,10 +3,13 @@
 GW_BIN="wso2am-micro-gw-toolkit-linux-3.1.0"
 #GW_BIN="wso2am-micro-gw-toolkit-3.0.1"
 
-echo "Downloading micro-gw"
-wget -q "https://github.com/wso2/product-microgateway/releases/download/v3.1.0/${GW_BIN}.zip"
+if [ ! -d $GW_BIN ]; then
+    echo "Downloading micro-gw"
+    wget -q "https://github.com/wso2/product-microgateway/releases/download/v3.1.0/${GW_BIN}.zip"
 
-unzip -q "${GW_BIN}.zip"
+    unzip -q "${GW_BIN}.zip"
+fi
+
 
 MGW_BASE_PATH="$PWD/$GW_BIN"
 MGW_BIN="$PWD/$GW_BIN/bin"

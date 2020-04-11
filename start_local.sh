9ca523ae7e5c04046c101613e962e9d10ff4ba46
Francisco Ribeiro <francisco@francisco.local>
2020-04-11 12:19:55 -0300

    Debug

--- start_local.sh
+++ start_local.sh
@@ -4,6 +4,6 @@ export analytics_fileUpload_uploadingEndpoint="https://localhost2:9444/analytics
 export analytics_fileUpload_rotatingPeriod=60000
 export analytics_fileUpload_task_uploadFiles=true
 export analytics_fileUpload_username="admin"
-export analytics_fileUpload_password="admin"
+export analytics_fileUpload_password="admin" 
 export b7a_log_level=DEBUG
 gateway myapi/target/myapi.jar
